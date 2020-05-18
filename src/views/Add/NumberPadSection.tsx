34a385306ff309385393b1e7d8a3e3da8a9095e1
wuxiao <1291383707@qq.com>
2020-05-18 10:50:48 +0800

    重构 NumberPadSection

--- src/views/Add/NumberPadSection.tsx
+++ src/views/Add/NumberPadSection.tsx
@@ -1,24 +1,6 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output{ background: #fff; font-size: 36px; line-height: 72px; text-align: right; padding: 0 16px;
-     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.5),//上阴影
-                 inset 0 5px 5px -5px rgba(0,0,0,0.5);// 下阴影
-  }
-  > .pad {
-     > button { font-size: 24px;float: left;width: 25%;height: 64px;border: none;
-     &.ok { height: 128px;float: right; }
-     &:nth-child(1){ background: #a8e6cf; }
-     &:nth-child(2), &:nth-child(5){ background: #dcedc1; }
-     &:nth-child(3), &:nth-child(6), &:nth-child(9){ background: #ffd3b6; }
-     &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(13){ background: #ffaaa5; }
-     &:nth-child(12), &:nth-child(15){ background: #f19292; }
-     }
-  }
-`;
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
@@ -33,40 +15,9 @@ const NumberPadSection: React.FC = () => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if(text === null){return;}
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) {return;}
-        setOutput(output + '.');
-        break;
-      case '退格':
-        if (output.length === 1) {
-          setOutput('');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('')
-        break;
-      case '保存':
-        console.log('保存')
-        break;
+    if(text ===  '保存'){return;}
+    if('123456789.'.split('').concat(['退格','清空']).indexOf(text) >= 0){
+        setOutput(generateOutput(text, output))
     }
   }
   return (

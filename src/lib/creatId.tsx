8a00902686fd0ffb8e3bcb522bc615667b9787f4
wuxiao <1291383707@qq.com>
2020-05-19 19:46:03 +0800

    重构：所有tag Id 都由 creatId 生成

---
+++ src/lib/creatId.tsx
@@ -0,0 +1,6 @@
+let id = 0;
+const creatId = () => {
+  id += 1;
+  return id;
+}
+export {creatId};
--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
+import {creatId} from './lib/creatId';
 
 const useTags = () => { // 封装自定义 Hook
   const [tags, setTags] = useState<{id: number; name: string}[]>([
-    {id: 1, name: '餐饮'},
-    {id: 2, name: '住宿'},
-    {id: 3, name: '服装'},
-    {id: 4, name: '交通'}
+    {id: creatId(), name: '餐饮'},
+    {id: creatId(), name: '住宿'},
+    {id: creatId(), name: '服装'},
+    {id: creatId(), name: '交通'}
   ])
   return{
     tags,setTags

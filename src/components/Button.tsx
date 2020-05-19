85a0821dac8698ab792aaaa6670b7cc592b58aa0
wuxiao <1291383707@qq.com>
2020-05-19 21:09:59 +0800

    重构： 抽离Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+font-size: 18px;border: none;padding: 8px 12px;
+border-radius: 5px;background: coral;color: #fff;
+`;
+
+export {Button}

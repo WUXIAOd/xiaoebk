0054d2ce77e6bf299b512805f3b757809f772046
wuxiao <1291383707@qq.com>
2020-05-19 23:23:09 +0800

    编辑 tag 名功能完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,7 +11,25 @@ const defaultId = [
 const useTags = () => { // 封装自定义 Hook
   const [tags, setTags] = useState<{id: number; name: string}[]>(defaultId)
   const findTag = (id:number) => tags.filter(tag => tag.id === id)[0]
+  const findTagIndex = (id:number) => {
+    let result = -1;
+    for(let i=0; i<tags.length; i++){
+      if(tags[i].id === id){
+        result = i;
+        break;
+      }
+    }
+    return result;
+  }
+  const updateTag = (id:number, obj: {name:string}) => {
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把tagsClone 得第index删掉，换成(id：id，name:obj.name)
+    tagsClone.splice(index, 1,{id:id,name:obj.name});
+    setTags(tagsClone)
+  }
   return{
-    tags,setTags, findTag }
+    tags,setTags, findTag, updateTag, findTagIndex }
 }
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -29,19 +29,21 @@ type Params = {
   id: string
 }
 const Tag: React.FC = () => {
-  const {findTag} = useTags();
-  const {id} = useParams<Params>();
-  const tag = findTag(parseInt(id));
+  const {findTag,updateTag} = useTags();
+  const {id: idString} = useParams<Params>();
+  const tag = findTag(parseInt(idString));
   return (
     <Layout>
       <Topbar>
         <Icon name="left"></Icon>
         <span>编辑标签</span>
-        <span></span>
+        <Icon/>
       </Topbar>
       <InputWrapper>
         <Input label="标签名：" type="text" placeholder="标签名"
-         value={tag.name}
+         value={tag.name} onChange={(e)=>{
+           updateTag(tag.id,{name:e.target.value});
+        }}
         />
       </InputWrapper>
       <Center>

9740629db3b454fe7601cebd82e8f65c645404a6
wuxiao <1291383707@qq.com>
2020-05-19 19:34:41 +0800

    重构 tag: string 重构为 tag: {id: number,name:string}

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,12 @@
 import {useState} from 'react';
 
-const useTags = () => {
-  const [tags, setTags] = useState<string[]>(['餐饮','住宿','服装','交通'])
+const useTags = () => { // 封装自定义 Hook
+  const [tags, setTags] = useState<{id: number; name: string}[]>([
+    {id: 1, name: '餐饮'},
+    {id: 2, name: '住宿'},
+    {id: 3, name: '服装'},
+    {id: 4, name: '交通'}
+  ])
   return{
     tags,setTags
   }
--- src/views/Add/TagsSection.tsx
+++ src/views/Add/TagsSection.tsx
@@ -3,34 +3,34 @@ import React from 'react';
 import {useTags} from 'useTags';
 
 type Props = {
-  value: string[];
-  onChange: (selected: string[]) => void;
+  value: number[];
+  onChange: (selected: number[]) => void;
 }
 const TagsSection: React.FC<Props> = (props) => {
   const {tags,setTags} = useTags()
-  const selectedTags = props.value;
+  const selectedTagIds = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('新标签名为：');
     if(tagName !== null){
-      setTags([...tags, tagName])
+      setTags([...tags, { id: Math.random(), name:tagName}]);
     }
   };
-  // 如果 tag 已被选中，就复制没有被选中的 tag ，作为新的 selectedTags
-  const onToggleTag = (tag: string) => {
-    const index = selectedTags.indexOf(tag)
+  // 如果 tag 已被选中，就复制没有被选中的 tag ，作为新的 selectedTagIds
+  const onToggleTag = (tagId: number) => {
+    const index = selectedTagIds.indexOf(tagId)
     if(index >= 0){
-      props.onChange(selectedTags.filter(t => t !== tag))
+      props.onChange(selectedTagIds.filter(t => t !== tagId))
     }else{
-      props.onChange([...selectedTags,tag])
+      props.onChange([...selectedTagIds,tagId])
     }
   };
-  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+  const getClass = (tagId: number) => selectedTagIds.indexOf(tagId) >= 0 ? 'selected' : ''
   return (
     <Wrapper>
       <ol>
         {tags.map(tag =>
-          <li key={tag} onClick={()=>{onToggleTag(tag)}}
-              className={ getClass(tag) }>{tag}</li>
+          <li key={tag.id} onClick={()=>{onToggleTag(tag.id)}}
+              className={ getClass(tag.id) }>{tag.name}</li>
         )}
       </ol>
       <button onClick={onAddTag}>添加标签</button>

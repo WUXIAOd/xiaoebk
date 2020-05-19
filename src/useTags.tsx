0d3a7c41cffa102fd980ecfd42d37a2bc158497b
wuxiao <1291383707@qq.com>
2020-05-19 23:52:51 +0800

    删除tag 功能完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -20,16 +20,24 @@ const useTags = () => { // 封装自定义 Hook
       }
     }
     return result;
-  }
+  };
   const updateTag = (id:number, obj: {name:string}) => {
     const index = findTagIndex(id);
+    // react 不允许修改原数组
     // 深拷贝 tags 得到 tagsClone
     const tagsClone = JSON.parse(JSON.stringify(tags));
+      // 把tagsClone 得第index删掉，换成(id：id，name:obj.name)
+      tagsClone.splice(index, 1,{id:id,name:obj.name});
+    setTags(tagsClone)
+  };
+  const deleteTag = (id: number) => {
+    const index = findTagIndex(id);
+    const tagsClone = JSON.parse(JSON.stringify(tags));
     // 把tagsClone 得第index删掉，换成(id：id，name:obj.name)
-    tagsClone.splice(index, 1,{id:id,name:obj.name});
+    tagsClone.splice(index, 1);
     setTags(tagsClone)
   }
   return{
-    tags,setTags, findTag, updateTag, findTagIndex }
+    tags,setTags, findTag, updateTag, findTagIndex, deleteTag }
 }
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -29,9 +29,25 @@ type Params = {
   id: string
 }
 const Tag: React.FC = () => {
-  const {findTag,updateTag} = useTags();
+  const {findTag, updateTag, deleteTag} = useTags();
   const {id: idString} = useParams<Params>();
   const tag = findTag(parseInt(idString));
+  const tagContent = (tag: { id: number, name: string }) => (
+    <div>
+      <InputWrapper>
+        <Input label="标签名：" type="text" placeholder="标签名"
+               value={tag.name} onChange={(e) => {
+          updateTag(tag.id, {name: e.target.value});
+        }}
+        />
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+      </Center>
+    </div>
+  );
   return (
     <Layout>
       <Topbar>
@@ -39,18 +55,7 @@ const Tag: React.FC = () => {
         <span>编辑标签</span>
         <Icon/>
       </Topbar>
-      <InputWrapper>
-        <Input label="标签名：" type="text" placeholder="标签名"
-         value={tag.name} onChange={(e)=>{
-           updateTag(tag.id,{name:e.target.value});
-        }}
-        />
-      </InputWrapper>
-      <Center>
-        <Space/>
-        <Space/>
-        <Button>删除标签</Button>
-      </Center>
+      {tag ? tagContent(tag) : <Center>tag不存在</Center>}
     </Layout>
   );
 };
